580e49aaca8d91db76a4fb73b09c889236686845
ASUS <ASUS@10.12.26.72>
2022-09-06 14:14:35 +0530

    new sort

---
+++ src/sorting/newsort.java
@@ -0,0 +1,13 @@
+package sorting;
+
+public class newsort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 225, n2 = 115, diff;
+		diff = n1 - n2;
+		System.out.println("the diff is: "+ diff);
+	}
+	
+
+}
